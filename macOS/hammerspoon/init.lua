97577c0faec3fc731ed2c330362ee0babb839181
Az <az.dev@e.mail.de>
2026-02-17 13:04:14 +0100

    Add new application shortcuts and improve quit logic

--- macOS/hammerspoon/init.lua
+++ macOS/hammerspoon/init.lua
@@ -1,35 +1,40 @@
 -- Application Shortcuts
-hs.hotkey.bind({'alt'}, '1', function() hs.application.launchOrFocus("Finder") end)
-hs.hotkey.bind({'alt'}, '2', function() hs.application.launchOrFocus("Firefox") end)
-hs.hotkey.bind({'alt'}, '3', function() hs.application.launchOrFocus("Google Chrome") end)
-hs.hotkey.bind({'alt'}, '4', function() hs.application.launchOrFocus("Insomnia") end)
-hs.hotkey.bind({'alt'}, 'q', function() hs.application.launchOrFocus("Terminal") end)
-hs.hotkey.bind({'alt'}, 'w', function() hs.application.launchOrFocus("Visual Studio Code") end)
-hs.hotkey.bind({'alt'}, 'e', function() hs.application.launchOrFocus("IntelliJ IDEA") end)
+hs.hotkey.bind({"shift","alt","ctrl"}, '1', function() hs.application.launchOrFocus("Microsoft Outlook") end)
+hs.hotkey.bind({"shift","alt","ctrl"}, '2', function() hs.application.launchOrFocus("Microsoft Teams") end)
+hs.hotkey.bind({"shift","alt","ctrl"}, 'q', function() hs.application.launchOrFocus("Terminal") end)
+hs.hotkey.bind({"shift","alt","ctrl"}, 'w', function() hs.application.launchOrFocus("Firefox") end)
+hs.hotkey.bind({"shift","alt","ctrl"}, 'e', function() hs.application.launchOrFocus("Google Chrome") end)
+hs.hotkey.bind({"shift","alt","ctrl"}, 's', function() hs.application.launchOrFocus("Sublime Text") end)
+hs.hotkey.bind({"shift","alt","ctrl"}, 'd', function() hs.application.launchOrFocus("IntelliJ IDEA") end)
+hs.hotkey.bind({"shift","alt","ctrl"}, 'y', function() hs.application.launchOrFocus("Orbstack") end)
+hs.hotkey.bind({"shift","alt","ctrl"}, 'x', function() hs.application.launchOrFocus("Insomnia") end)
+hs.hotkey.bind({"shift","alt","ctrl"}, 'c', function() hs.application.launchOrFocus("Finder") end)
 
-hs.hotkey.bind({'alt'}, 't', function() hs.application.launchOrFocus("Microsoft Teams") end)
-hs.hotkey.bind({'alt'}, 'z', function() hs.application.launchOrFocus("Slack") end)
-hs.hotkey.bind({'alt'}, 'o', function() hs.application.launchOrFocus("Microsoft Outlook") end)
-hs.hotkey.bind({'alt'}, 'p', function() hs.application.launchOrFocus("Passwords") end)
+hs.hotkey.bind({"shift", "cmd"}, 'q', function() hs.application.frontmostApplication():kill() end)
 
 -- Close window or quit application Shortcut
 hs.hotkey.bind({"cmd"}, "q", function()
 
-    local focusedApp = hs.application.frontmostApplication()
+    local frontmostApp = hs.application.frontmostApplication()
     local focusedWindow = hs.window.focusedWindow()
 
-    if focusedApp and #focusedApp:allWindows() <= 1 and focusedApp:name() ~= "Finder"  then
-        focusedApp:kill()
+    if frontmostApp
+        and #frontmostApp:allWindows() <= 1
+        and frontmostApp:name() ~= "Finder"
+        and frontmostApp:name() ~= "OrbStack"
+    then
+        frontmostApp:kill()
     elseif focusedWindow then
         focusedWindow:close()
     end
 end)
 
 -- Reload Config Shortcut
-hs.hotkey.bind({"alt"}, "r", function()
+hs.hotkey.bind({"shift","alt","ctrl"}, "return", function()
     hs.reload()
 end)
 
 -- Notify on successful reload
 hs.alert.show("config loaded")
+
 
